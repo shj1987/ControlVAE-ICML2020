42f75f3ab19d2e6288d8bff628bf7d9b9d9a7745
shj1987 <huajieshao1987@gmail.com>
2023-06-02 12:17:08 +0000

    update combine

--- Disentangling/combine_recon.py
+++ Disentangling/combine_recon.py
@@ -5,7 +5,8 @@ from PIL import Image
 import glob
 from torchvision.utils import make_grid, save_image
 import re
-import sys,os
+import sys
+import os
 
 # %%
 def atoi(text):
@@ -14,47 +15,32 @@ def atoi(text):
 def natural_keys(text):
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
-
 # %%
 def combineImages(name_initial, indices):
     padding = 2
-
-    recon = np.rollaxis(np.asarray(Image.open('recon.jpg'))[padding:-padding, padding:-padding, :], 2, 0) / 255.0
-    recon_allwidth = recon.shape[1]
-    origin = recon[:, :, 0:recon_allwidth]
-    recon = recon[:, :, (recon_allwidth + padding):]
-    grid_size = 8
-    recon_width = recon_allwidth / grid_size
-
     images = [np.rollaxis(np.asarray(Image.open(f))[padding:-padding, padding:-padding, :], 2, 0) / 255.0 \
         for f in sorted(glob.glob('{}_[0-9].jpg'.format(name_initial)) \
             + glob.glob('{}_[0-9][0-9].jpg'.format(name_initial)), key=natural_keys)]
     width = images[0].shape[1] + padding
     steps = len(images)
     allt = []
-    for i in range(steps):
-        hfrom = int((i // grid_size) * recon_width)
-        wfrom = int((i % grid_size) * recon_width)
-        allt.append(origin[:, hfrom:(hfrom + width - padding), wfrom:(wfrom + width - padding)])
-    for i in range(steps):
-        hfrom = int((i // grid_size) * recon_width)
-        wfrom = int((i % grid_size) * recon_width)
-        allt.append(recon[:, hfrom:(hfrom + width - padding), wfrom:(wfrom + width - padding)])
     for index in indices:
         for image in images:
             allt.append(image[:, :, (index * width):((index + 1) * width - padding)])
-    print(allt[0].shape)
-    save_image(tensor=torch.tensor(allt), filename='result/{}_combined.eps'.format(name_initial), nrow=steps, pad_value=1)
+    save_image(tensor=torch.tensor(allt), fp='result/{}_combined.eps'.format(name_initial), nrow=steps, pad_value=1)
     
 
 # %%
 # combineImages('fixed_heart', [2, 4, 5, 6, 9, 8])
+# label_lst = ['z1 (y)','z2','z3','z4','z5','z6 (Orientation)','z7 (Shape)',\
+                # 'z8 (x)','z9','z10 (Scale)','total KL']
 
 # %%
 if __name__ == '__main__':
     directory = 'result'
     if not os.path.exists(directory):
         os.makedirs(directory)
-    combineImages(sys.argv[1], [int(x) for x in sys.argv[2:]])
+    # combineImages(sys.argv[1], [int(x) for x in sys.argv[2:]])
+    # combineImages('fixed_ellipse', [0,1,2,3,4,5,6,7,8,9])
+    combineImages('fixed_ellipse', [0,6])
     
-# %%
